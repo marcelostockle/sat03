65b7ec911c6310fd1a4e51723e82ee8e8d8ea57b
marcelostockle <marcelostockle@hotmail.com>
2022-12-03 18:23:26 -0300

    Style changes

--- src/MiddleModal.jsx
+++ src/MiddleModal.jsx
@@ -1,16 +1,27 @@
 import React, { useEffect, useState } from 'react'
 import { AiFillCloseSquare } from 'react-icons/ai'
 import { useModalContext } from './ModalContext'
+import styled from 'styled-components'
 import './MiddleModal.css'
 
 const MiddleModal = () => {
   const {modal, closeModal} = useModalContext()
+  const Title = styled.h3`
+    text-align: center;
+    border-style: solid;
+    border-color: blue;
+    border-radius: 15px;
+  `
+  const ContainedImg = styled.img`
+    max-height: 40vh;
+    object-fit: contain;
+  `
   return (
     <div className='parent-container' style={{display: modal.active ? 'flex' : 'none'}}>
       <AiFillCloseSquare className='close-btn' size={'2em'} onClick={() => closeModal()}/>
       <div className='modal-container'>
-        <h3 style={{textAlign: 'center'}}>{modal.title}</h3>
-        <img src={modal.pic}/>
+        <Title>{modal.title}</Title>
+        <ContainedImg src={modal.pic}/>
         <p style={{textAlign: 'justify'}}>{modal.text}</p>
       </div>
     </div>
